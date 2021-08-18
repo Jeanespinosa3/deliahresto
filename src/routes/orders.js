ac424192273281149448592fa44700f042463b0b
Jean Espinosa <tjleviss@gmail.com>
2021-08-18 04:53:57 -0500

    Se agrego el endpoint post para las ordenes de pedidos

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,25 +1,58 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const actions = require("../database/actions")
 
-router.get('/orders', (req, res)=> {
-    //Code here
-    res.send('no hay ordenes');
-});
+router.get("/orders", (req, res) => {
+  //Code here
+  res.send("no hay ordenes")
+})
 
-router.get('/order/:id', (req, res)=> {
-    //Code here
-});
+router.get("/order/:id", (req, res) => {
+  //Code here
+})
 
-router.post('/order', (req, res)=> {
-    //Code here
-});
+router.post("/order", async (req, res) => {
+  const bodyOrders = req.body
+  const orderInfo = bodyOrders.order
+  const ordersDescriptionInfo = bodyOrders.orderDescription
 
-router.put('/order/:id', (req, res)=> {
-    //Code here
-});
+  const result = await actions.Insert("INSERT INTO orders (DateTime, Payment, IdUser, State) VALUES (NOW(), :Payment, :IdUser, :State)", orderInfo)
+  const idOrder = result[0]
+  console.log(idOrder)
 
-router.delete('/order/:id', (req, res)=> {
-    //Code here
-});
+  for (const orderDescriptionInfo of ordersDescriptionInfo) {
+    await actions.Insert("INSERT INTO orderdetails (IdOrder, IdProduct, Amount) VALUES (:idOrder, :IdProduct, :Amount)", { idOrder, ...orderDescriptionInfo })
+  }
 
-module.exports = router;
+  const resultQueryName = await actions.Select(
+    `
+    SELECT SUM(p.Price * do.Amount) as total,
+    GROUP_CONCAT(do.Amount, "x ", p.Name, " ") as Description
+    FROM orderdetails do
+    INNER JOIN products p ON (p.Id = do.IdProduct)
+    WHERE do.IdOrder = :idOrder`,
+    { idOrder }
+  )
+
+  const resultOrderUpdate = await actions.Update(
+    `UPDATE orders 
+    SET Description = :Description, Total = :total WHERE Id = :idOrder`,
+    { idOrder, Description: resultQueryName[0].Description, total: resultQueryName[0].total }
+  )
+
+  if (resultOrderUpdate.error) {
+    res.status(500)
+  } else {
+    res.json(resultOrderUpdate)
+  }
+})
+
+router.put("/order/:id", (req, res) => {
+  //Code here
+})
+
+router.delete("/order/:id", (req, res) => {
+  //Code here
+})
+
+module.exports = router
