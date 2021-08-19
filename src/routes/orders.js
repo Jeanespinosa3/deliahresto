6e6787b40c57d6c729fc0b5e5207a0b28dc0caf3
Jean Espinosa <tjleviss@gmail.com>
2021-08-19 16:42:52 -0500

    Validacion token y administrador para crear una orden

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -38,63 +38,48 @@ router.get("/orders", auth.auth, auth.validateAdmi, async (req, res) => {
   }
 })
 
-router.post("/order", async (req, res) => {
+router.post("/order", auth.auth, auth.validateAdmi, async (req, res) => {
+  const isAdmi = req.admi
   const bodyOrders = req.body
   const orderInfo = bodyOrders.order
   const ordersDescriptionInfo = bodyOrders.orderDescription
-
-  const result = await actions.Insert("INSERT INTO orders (DateTime, Payment, IdUser, State) VALUES (NOW(), :Payment, :IdUser, :State)", orderInfo)
-  const idOrder = result[0]
-  console.log(idOrder)
-
-  for (const orderDescriptionInfo of ordersDescriptionInfo) {
-    await actions.Insert("INSERT INTO orderdetails (IdOrder, IdProduct, Amount) VALUES (:idOrder, :IdProduct, :Amount)", { idOrder, ...orderDescriptionInfo })
-  }
-
-  const orderName = await actions.Select(
-    `
-    SELECT SUM(p.Price * do.Amount) as total,
-    GROUP_CONCAT(do.Amount, "x ", p.Name, " ") as Description
-    FROM orderdetails do
-    INNER JOIN products p ON (p.Id = do.IdProduct)
-    WHERE do.IdOrder = :idOrder`,
-    { idOrder }
-  )
-
-  const resultOrderUpdate = await actions.Update(
-    `UPDATE orders 
-    SET Description = :Description, Total = :total WHERE Id = :idOrder`,
-    { idOrder, Description: orderName[0].Description, total: orderName[0].total }
-  )
-
-  if (resultOrderUpdate.error) {
-    res.status(500)
-  } else {
-    res.status(200).json({ success: true, orderName, message: "Your order have been accepted" })
-  }
-})
-
-router.patch("/order/:id", auth.auth, auth.validateAdmi, middlewares.validateBodyStateOrder, async (req, res) => {
-  const isAdmi = req.admi
-  const params = req.body
-  const resultQuery = await actions.Select("SELECT * FROM orders WHERE Id = :id", { id: req.params.id })
-  let result
-
   if (isAdmi === 1) {
     try {
-      result = await actions.Update(`UPDATE orders SET State =:State WHERE Id =${req.params.id}`, params)
+      const result = await actions.Insert("INSERT INTO orders (DateTime, Payment, IdUser, State) VALUES (NOW(), :Payment, :IdUser, :State)", orderInfo)
+      const idOrder = result[0]
+      console.log(idOrder)
+
+      for (const orderDescriptionInfo of ordersDescriptionInfo) {
+        await actions.Insert("INSERT INTO orderdetails (IdOrder, IdProduct, Amount) VALUES (:idOrder, :IdProduct, :Amount)", { idOrder, ...orderDescriptionInfo })
+      }
+
+      const orderName = await actions.Select(
+        `
+      SELECT SUM(p.Price * do.Amount) as total,
+      GROUP_CONCAT(do.Amount, "x ", p.Name, " ") as Description
+      FROM orderdetails do
+      INNER JOIN products p ON (p.Id = do.IdProduct)
+      WHERE do.IdOrder = :idOrder`,
+        { idOrder }
+      )
+
+      const resultOrderUpdate = await actions.Update(
+        `UPDATE orders 
+      SET Description = :Description, Total = :total WHERE Id = :idOrder`,
+        { idOrder, Description: orderName[0].Description, total: orderName[0].total }
+      )
+
+      if (resultOrderUpdate.error) {
+        res.status(500)
+      } else {
+        res.status(200).json({ success: true, orderName, message: "Your order have been accepted" })
+      }
     } catch (error) {
       res.status(500).json(error)
     }
-
-    if (!resultQuery.length) {
-      res.status(404).json({ succes: false, message: "Order not found" })
-    } else {
-      res.status(200).json({ succes: true, message: "Order has been updated" })
-    }
   } else {
     res.json({
-      error: "This user has not authorization for make this request",
+      error: "This user has not authorization for make this request ",
       codeError: 01,
     })
   }
